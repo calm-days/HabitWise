22ede134654109f1738a666d93728ffa3798c5ae
Roman Liukevich <r.liukevich@gmail.com>
2022-09-14 18:49:13 +0200

    Initial Commit

---
+++ HabitWise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitWise
+//
+//  Created by Roman Liukevich on 9/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HabitWise/HabitWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitWiseApp.swift
+//  HabitWise
+//
+//  Created by Roman Liukevich on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
